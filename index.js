f3997b9f9a17da4f4cba62f00e73c69845b135f3
Pramudya <luckcarrot@gmail.com>
2023-11-02 15:15:59 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var numberOfDrumButton = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i < numberOfDrumButton; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+        this.style.color = "white";
+        
+    })
+}
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
